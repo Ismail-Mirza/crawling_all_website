a5699f13a3de794a2e7a9fc31e9a0094742ff8d3
Mohammad Ismail Mirza <67634789+Ismail-Mirza@users.noreply.github.com>
2023-11-23 23:00:30 +0000

    inital

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+csv_file_path = "links.csv"
+for page in [1]:
+
+    url = f'http://oldweb.lged.gov.bd/GovtWebsites.aspx'
+    grab = requests.get(url)
+    soup = BeautifulSoup(grab.text, 'html.parser')
+
+    # Extract all links
+    links = [link.get('href') for link in soup.find_all("a")]
+
+    # Write links to CSV file
+
+    with open(csv_file_path, "a", newline="", encoding="utf-8") as csv_file:
+        csv_writer = csv.writer(csv_file)
+
+        # Check if the link is not None and is not a relative path
+        for link in links:
+            if link and not link.startswith("#") and not link.startswith("mailto:"):
+                print(link)
+                csv_writer.writerow([link])
+
+print(f"All links have been appended to {csv_file_path}")
