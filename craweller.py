23d3993524b0567623f2ce85e9f8e6ca5a984ad6
Mohammad Ismail Mirza <67634789+Ismail-Mirza@users.noreply.github.com>
2023-11-24 07:38:24 +0000

    message

---
+++ craweller.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+from pdf import scrape_and_download
+import os
+
+csv_file_path = "links.csv"
+
+
+
+with open(csv_file_path, newline="", encoding="utf-8") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        website=row[0].strip("/")
+        if website.startswith("http://") or website.startswith("www.") or website.startswith("https://"):
+            # print(website)
+
+            # pass
+            try:
+                grab = requests.get(website,verify=False)
+                soup = BeautifulSoup(grab.text, 'html.parser')
+
+                # Extract all links
+            except requests.exceptions.RequestException as e:
+                print('error in ',website)
+            links = [link.get('href') for link in soup.find_all("a")]
+
+
+
+            for link in links:
+                if link and not link.startswith("#") and not link.startswith("http://") and not link.startswith("https://")  and not link.startswith("mailto:"):
+                    root_folder = 'downloaded_files'
+                    website_url = website +'/'+ link.strip("/")
+                    print("downloading from "+website_url)
+                    if not os.path.exists(root_folder):
+                        os.makedirs(root_folder)
+                    output_folder = root_folder + "/"+website
+                    if not os.path.exists(output_folder):
+                        os.makedirs(output_folder)
+                    scrape_and_download(website_url, output_folder)
+
+                    
+                    
+                    
+                    
+        else:
+            print("website not in ",website)
+    
--- pdf.py
+++ pdf.py
@@ -8,8 +8,15 @@ def download_file(url, folder_path):
     if response.status_code == 200:
         file_name = url.split("/")[-1]
         file_path = os.path.join(folder_path, file_name)
-        with open(file_path, 'wb') as f:
-            f.write(response.content)
+        try:
+            with open(file_path, 'wb') as f:
+                f.write(response.content)
+        except OSError as oserr:
+            print("error in downloading file")
+
+
+
+
         print(f"Downloaded: {file_name}")
         return file_path
     else:
@@ -17,29 +24,32 @@ def download_file(url, folder_path):
         return None
 
 def scrape_and_download(url, folder_path):
-    grab = requests.get(url,verify=False)
-    soup = BeautifulSoup(grab.text, 'html.parser')
+    try: 
+        grab = requests.get(url,verify=False)
+        soup = BeautifulSoup(grab.text, 'html.parser')
 
-    for link in soup.find_all("a"):
-        href = link.get("href")
-        if href:
-            absolute_url = urljoin(url, href)
-            lower_url = absolute_url.lower()
+        for link in soup.find_all("a"):
+            href = link.get("href")
+            if href:
+                absolute_url = urljoin(url, href)
+                lower_url = absolute_url.lower()
 
-            if any(extension in lower_url for extension in ['.pdf', '.docx', '.doc', '.txt', '.ppt', '.pptx']):
-                download_file(absolute_url, folder_path)
-            else:
-                with open("non_downloadable_links.txt", "a") as non_dl_file:
-                    non_dl_file.write(absolute_url + "\n")
+                if any(extension in lower_url for extension in ['.pdf', '.docx', '.doc', '.txt', '.ppt', '.pptx']):
+                    download_file(absolute_url, folder_path)
+                else:
+                    with open("non_downloadable_links.txt", "a") as non_dl_file:
+                        non_dl_file.write(absolute_url + "\n")
+    except requests.exceptions.RequestException as e:
+        print('error in ',url)
 
 # Replace 'your_website_url' with the actual URL you want to scrape
-website_url = 'http://www.nctb.gov.bd/site/files/e977a7e5-be01-4c42-99ac-bc966d334ae7/-'
-output_folder = 'downloaded_files'
+# website_url = 'http://www.nctb.gov.bd/site/files/e977a7e5-be01-4c42-99ac-bc966d334ae7/-'
+# output_folder = 'downloaded_files'
 
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
+# if not os.path.exists(output_folder):
+#     os.makedirs(output_folder)
 
-# Scrape and download files
-scrape_and_download(website_url, output_folder)
+# # Scrape and download files
+# scrape_and_download(website_url, output_folder)
 
-print("Scraping and downloading complete.")
+# print("Scraping and downloading complete.")
